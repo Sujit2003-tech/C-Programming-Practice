233a4b8967157eacaeb6b1cf07839ad0275f90bc
Sujit2003-tech <250320100095@cutm.ac.in>
2025-09-01 00:38:26 +0530

    Delete C -Programming-Practice_Bank directory

--- C -Programming-Practice_Bank/Size_of_datatypes.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("The size of interger is %d\n",sizeof(int));
-printf("The size of float value is %d \n",sizeof(float));
-printf("The size of character value is %d \n",sizeof(char));
-
-    return 0;
-}
